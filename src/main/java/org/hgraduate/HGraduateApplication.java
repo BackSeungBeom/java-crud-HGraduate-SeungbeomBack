e1ca7260eef6e857e6fad95e77e7f62cfc9c8b13
BackSeungBeom <seungbeom1234@gmail.com>
2026-07-13 10:31:35 +0900

    init

---
+++ src/main/java/org/hgraduate/HGraduateApplication.java
@@ -0,0 +1,13 @@
+package org.hgraduate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HGraduateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HGraduateApplication.class, args);
+    }
+
+}
